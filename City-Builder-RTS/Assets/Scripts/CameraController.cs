c845cc47dca3b669e703135b3912f2da025417d4
Spjarmf <47174043+Spjarmf@users.noreply.github.com>
2021-06-10 15:22:40 +0200

    rotation

--- City-Builder-RTS/Assets/Scripts/CameraController.cs
+++ City-Builder-RTS/Assets/Scripts/CameraController.cs
@@ -7,33 +7,45 @@ public class CameraController : MonoBehaviour
     public float panSpeed = 11;
     public float scrollSpeed = 1;
     public float panThickness = 1;
+    public float rotationSpeed = 100;
+    public float lookSpeed = 10;
 
     public KeyCode forward = KeyCode.W;
     public KeyCode backward = KeyCode.S;
     public KeyCode left = KeyCode.A;
     public KeyCode right = KeyCode.D;
-    
+
+    public KeyCode rotateLeft = KeyCode.Q;
+    public KeyCode rotateRight = KeyCode.E;
+
+    public KeyCode mouse2 = KeyCode.Mouse2;
+
+
+    public LayerMask layerMask;
+
+    private Transform mainCamera;
+
 
     private Vector3 InputHelper() 
     {
         Vector3 direction = Vector3.zero;
 
-        if (Input.GetKey(forward) || Input.mousePosition.y >= 0 + panThickness) 
+        if (Input.GetKey(forward)  || Input.mousePosition.y >= Screen.height - panThickness) 
         {
             direction += Vector3.forward;
         }
 
-        if (Input.GetKey(backward) || Input.mousePosition.y <= Screen.height - panThickness)
+        if (Input.GetKey(backward) || Input.mousePosition.y <= 0 + panThickness)
         {
             direction += Vector3.back;
         }
 
-        if (Input.GetKey(left) || Input.mousePosition.x <= Screen.width - panThickness)
+        if (Input.GetKey(left)  || Input.mousePosition.x <= 0 + panThickness)
         {
             direction += Vector3.left;
         }
 
-        if (Input.GetKey(right) || Input.mousePosition.x >= 0 + panThickness)
+        if (Input.GetKey(right) || Input.mousePosition.x >= Screen.width - panThickness)
         {
             direction += Vector3.right;
         }
@@ -53,15 +65,65 @@ public class CameraController : MonoBehaviour
         return direction;
     }
 
+    void CameraRotation()
+    {
+        RaycastHit hit;
+
+        
+        if (Physics.Raycast(transform.position, mainCamera.forward, out hit, 200, layerMask))
+        {
+            if (Input.GetKey(rotateLeft))
+            {
+                transform.RotateAround(hit.point, Vector3.up, -rotationSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(rotateRight))
+            {
+                transform.RotateAround(hit.point, Vector3.up, rotationSpeed * Time.deltaTime);
+            }
+            //Debug.DrawRay(transform.position, mainCamera.forward * hit.distance, Color.yellow);
+        }
+    }
+
+    void CameraLook() 
+    {
+
+        if (Input.GetKey(mouse2)) 
+        {
+            print("mouse2");
+   
+            if (Input.GetAxis("Mouse Y") < 0)
+            {
+                mainCamera.rotation = mainCamera.rotation * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * lookSpeed, Vector3.left);
+            }
+
+            if (Input.GetAxis("Mouse Y") > 0)
+            {
+                mainCamera.rotation = mainCamera.rotation * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * -lookSpeed, Vector3.left);
+            }
+
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                mainCamera.rotation = mainCamera.rotation * Quaternion.AngleAxis(Input.GetAxis("Mouse X") * lookSpeed, Vector3.up);
+            }
+
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                mainCamera.rotation = mainCamera.rotation * Quaternion.AngleAxis(Input.GetAxis("Mouse X") * -lookSpeed, Vector3.up);
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = transform.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = transform.position + InputHelper();
+        CameraRotation();
+        CameraLook();
     }
 }
